106652137fd3eabd052db86eb4d57e18a8e124cb
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-02 18:13:10 -0600

    Create table dependencias
    Create table dependencias in the query

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -12,11 +12,22 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+        $this->pdo->query("CREATE TABLE 'Dependencias'(
+  'id'int(10) unsigned NOT NULL AUTO_INCREMENT,
+  'nombre'varchar(90) COLLATE latin1_general_ci DEFAULT NULL,
+  'estatus'enum('activa','inactiva') COLLATE latin1_general_ci DEFAULT NULL,
+  'prefijo'varchar(10) COLLATE latin1_general_ci DEFAULT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+");
+
     }
 
     public function tearDown()
     {
         $this->pdo->query("DROP TABLE hello");
+        $this->pdo->query("DROP TABLE Dependencias");
+
     }
 
     public function testHelloWorld()

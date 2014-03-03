2272840ff8759ed76976f3a3008e71598be93660
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-02 19:23:46 -0600

    Drop table dependencias
    Add "Drop If exist dependencias"

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -12,6 +12,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+        $this->pdo->query("DROP TABLE IF EXISTS Dependencias");
         $this->pdo->query("CREATE TABLE Dependencias (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   nombre varchar(90) COLLATE latin1_general_ci DEFAULT NULL,

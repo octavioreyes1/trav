8fa1ad9416a29e3af1458ab4e1f5262c5230635c
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-03 01:07:08 -0600

    Change the order of the Insert
    Change the order of the Insert

--- Tests/LoginDaoTest.php
+++ Tests/LoginDaoTest.php
@@ -19,9 +19,7 @@ class LoginDaoTest extends PHPUnit_Framework_TestCase
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 ");
-        $this->pdo->query("INSERT INTO Usuarios (id, nombre, paterno, materno, username, password, fechaAlta, estatus, idTipoUsuario) VALUES 
-  (10, 'Octavio', 'Reyes', 'Pinedo', 'Octavio', '958a74a4695ec722416c949165fd7c50', '2014-03-02', 'activo', 1)");
-
+        $this->pdo->query("INSERT INTO TipoUsuarios VALUES (1,'Administrador'),(2,'Telefonista')");
         $this->pdo->query("DROP TABLE IF EXISTS Usuarios");
         $this->pdo->query("CREATE TABLE Usuarios (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -38,8 +36,11 @@ class LoginDaoTest extends PHPUnit_Framework_TestCase
   CONSTRAINT fk_Usuarios_TipoUsuarios1 FOREIGN KEY (idTipoUsuario) REFERENCES TipoUsuarios (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 ");
+        $this->pdo->query("INSERT INTO Usuarios (id, nombre, paterno, materno, username, password, fechaAlta, estatus, idTipoUsuario) VALUES 
+  (10, 'Octavio', 'Reyes', 'Pinedo', 'Octavio', '958a74a4695ec722416c949165fd7c50', '2014-03-02', 'activo', 1)");
 
-        $this->pdo->query("INSERT INTO TipoUsuarios VALUES (1,'Administrador'),(2,'Telefonista')");
+
+
 
 
     }

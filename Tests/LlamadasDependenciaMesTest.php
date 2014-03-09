1993fe9dcc1fe44ade2e211228e5b93d8e5055de
therojo <guillermmorojcruz@gmail.com>
2014-03-09 16:17:45 -0600

    Test de reporte de llamadas por dependencia mes

---
+++ Tests/LlamadasDependenciaMesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Description of LlamadasDependenciaMesTest
+ *
+ * @author rojo
+ */
+
+require_once 'classes/Connection.php';
+require_once 'classes/LlamadaDao.php';
+require_once 'classes/Llamada.php';
+
+class LlamadasDependenciaMesTest extends PHPUnit_Framework_TestCase
+{
+  /**
+    * @var PDO
+    */
+   private $pdo;
+
+    public function setUp() {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+   
+   
+    public function tearDown() {
+    }
+   
+    public function testGetLlamadasDependenciaMes() {  
+      
+      // Creamos una instancia de conexion a la BD
+      $conn = new Connection();
+      $llamadaDao = new LlamadaDao($conn->getConexion());   
+      
+      $objLlamada=new Llamada(0);
+      $objLlamada->setExtension(3057);
+      $objLlamada->setFecha("2014-03-08");
+      $objLlamada->setUsuario(1);
+      $llamadaDao->addLlamada($objLlamada);
+      
+      // Ahora el test me debe regresar un registro, ya que es de una dependencia
+      
+      $arrDependencias=$llamadaDao->getLlamadasPorDependenciaMes();
+      
+      // El assert es que me regrese un array   
+      $this->assertEquals(1, count($arrDependencias));
+      
+    }
+}
+
+?>

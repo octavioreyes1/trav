62f129f6a51703152080ea2faebad65f4a433635
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-09 19:53:06 -0600

    Change the query (extensiones->Extensiones) in LllamadasByUserDao

--- classes/LlamadasByUserDao.php
+++ classes/LlamadasByUserDao.php
@@ -16,15 +16,20 @@ class LlamadasByUserDao {
 
 function findLlamadasByUser($idUsuario) {                 
       $sql="select Extensiones.numero as numero, Llamadas.fecha fecha, Dependencias.nombre dependencia from Llamadas inner join Extensiones on Llamadas.idExtension=Extensiones.id
-                 inner join Dependencias on Dependencias.id=extensiones.idDependencia where Llamadas.idUsuario=$idUsuario limit 0, 10";                 
-      $result = mysqli_query($this->connDb, $sql);                    
-      $resultado = array();
+                 inner join Dependencias on Dependencias.id=Extensiones.idDependencia where Llamadas.idUsuario=$idUsuario limit 0, 10";                 
+      $result = mysqli_query($this->connDb, $sql);
+      
       if ($result){
+      $resultado = array();
       while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
               $resultado[] = array ( 'numero' => $row['numero'],
                                      'fecha' => $row['fecha'],
                                      'dependencia' => $row['dependencia']);
        }      
+      }
+
+      if (false===$result) {
+            $resultado="Fallo";
       }
       return $resultado;
    }

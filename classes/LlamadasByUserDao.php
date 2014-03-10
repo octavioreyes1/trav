6eec0cc7c56b694c33efe31b5c8f077df44cf003
octavioreyes1 <octavioreyes1@hotmail.com>
2014-03-09 18:21:30 -0600

    Updated LlamadasByUserDao (add an IF)

--- classes/LlamadasByUserDao.php
+++ classes/LlamadasByUserDao.php
@@ -19,12 +19,13 @@ function findLlamadasByUser($idUsuario) {
                  inner join Dependencias on Dependencias.id=extensiones.idDependencia where Llamadas.idUsuario=$idUsuario limit 0, 10";                 
       $result = mysqli_query($this->connDb, $sql);                    
       $resultado = array();
-      while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-       {
+      if ($result){
+      while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
               $resultado[] = array ( 'numero' => $row['numero'],
-                    'fecha' => $row['fecha'],
-                    'dependencia' => $row['dependencia']);
-}      
+                                     'fecha' => $row['fecha'],
+                                     'dependencia' => $row['dependencia']);
+       }      
+      }
       return $resultado;
    }
    

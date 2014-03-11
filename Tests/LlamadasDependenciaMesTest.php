ddd3b10a39a96600fe33fcc0dcbfcf4365364bc5
therojo <guillermmorojcruz@gmail.com>
2014-03-10 19:28:37 -0600

    Llamadas por Dependencia mes

--- Tests/LlamadasDependenciaMesTest.php
+++ Tests/LlamadasDependenciaMesTest.php
@@ -33,10 +33,10 @@ class LlamadasDependenciaMesTest extends PHPUnit_Framework_TestCase
       $llamadaDao = new LlamadaDao($conn->getConexion());   
       
       $objLlamada=new Llamada(0);
-      $extension=3057;
+      $extension=3056;
       $objLlamada->setFecha("2014-03-08");
       $objLlamada->setUsuario(1);
-      $llamadaDao->addLlamada($objLlamada,$extension);
+      $llamadaDao->insertLlamada($objLlamada,$extension);
       
       // Ahora el test me debe regresar un registro, ya que es de una dependencia
       

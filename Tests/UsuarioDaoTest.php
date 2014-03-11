25e989a45a1b3b6350f85a0b7cc0a3d6fbadb7fc
therojo <guillermmorojcruz@gmail.com>
2014-03-10 23:56:47 -0600

    Cambio el assert en change password

--- Tests/UsuarioDaoTest.php
+++ Tests/UsuarioDaoTest.php
@@ -108,7 +108,7 @@ class UsuarioDaoTest extends PHPUnit_Framework_TestCase {
      $usuarioDao = new UsuarioDao($conn->getConexion());
      $objUsuario=$usuarioDao->findById(1);
      
-     $this->assertEquals(true,$usuarioDao->changePassword($objUsuario, 'perez','perez', 'perez'));
+     $this->assertEquals(1,$usuarioDao->changePassword($objUsuario, 'perez','perez', 'perez'));
    }
    
 }

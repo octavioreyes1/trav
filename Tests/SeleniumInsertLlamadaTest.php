5145b3a875e59e9eb2d43f5fe97fe0fec4cd7768
therojo <guillermmorojcruz@gmail.com>
2014-03-10 21:29:00 -0600

    SeleniumInsertLlamadaTest.php para metodo insert llamada

---
+++ Tests/SeleniumInsertLlamadaTest.php
@@ -0,0 +1,27 @@
+<?php
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://www.tourzac.com/callCenter/index.php");
+  }
+
+  public function testMyTestCase()
+  {
+    $this->open("/callCenter/index.php");
+    $this->type("name=txtUsername", "chabela");
+    $this->type("name=txtPassword", "perez");
+    $this->click("css=input[type=\"submit\"]");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Registrar Llamada");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=txtExtension", "3056");
+    $this->click("css=input[type=\"submit\"]");
+    $this->waitForPageToLoad("30000");
+    $this->verifyText("css=font", "Se registro la llamada 3056");
+    $this->click("css=img[title=\"Cerrar Sesion\"]");
+    $this->waitForPageToLoad("30000");
+  }
+}
+?>
